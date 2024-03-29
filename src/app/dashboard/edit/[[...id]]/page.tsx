584eed4283a8c9810963603413866280a577e173
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-03-29 20:51:51 +0000

    Fix crutial error

--- src/app/dashboard/edit/[[...id]]/page.tsx
+++ src/app/dashboard/edit/[[...id]]/page.tsx
@@ -1,11 +1,14 @@
 import FormCard from "@/components/FormCard";
 import {getPassword} from "@/app/actions";
 import {redirect} from "next/navigation";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
 const page = async ({params}:{params:{id:number}}) => {
     const password = await getPassword(params.id)
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
 
-    if (!password) redirect('/dashboard')
+    if (!password || user?.id !== password.userId) redirect('/dashboard')
 
     return (
         <div className=' flex items-center justify-center pt-6 h-screen'>
